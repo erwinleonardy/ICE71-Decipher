4b3898d4a8636a295ab652be829bcc1c1c1b9172
erwinleonardy <erwin.leonardy@yahoo.com>
2019-04-13 14:21:51 +0800

    Set a REST endpoint for backend

--- crawler.py
+++ crawler.py
@@ -1,4 +1,4 @@
 from crawler import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- crawler/driver.py
+++ crawler/driver.py
@@ -2,7 +2,6 @@ from flask import jsonify
 
 class Driver():
     @staticmethod
-    def arne_is_great():
-        text = "Arne is superb"
-        return text
-        # return jsonify(text)
+    def arne_is_gay():
+        text = "Arne is gay and bachelor"
+        return jsonify({'Arne the Project Manager who does nothing': text})
