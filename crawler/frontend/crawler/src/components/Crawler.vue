be94c805ae90927391ff19f12dba9543fe25d504
Supreme-leader-of-North-Korea <37832470+Supreme-leader-of-North-Korea@users.noreply.github.com>
2019-04-13 19:14:22 +0800

    no message

--- crawler/frontend/crawler/src/components/Crawler.vue
+++ crawler/frontend/crawler/src/components/Crawler.vue
@@ -191,7 +191,7 @@
 
 
                 const config = {
-                    headers: { 'Access-Control-Allow-Origin': '*' }
+                    headers: { 'Access-Control-Allow-Origin': '*' , 'content-type': 'application/json; charset=utf-8' }
                 };
 
                 axios.post("http://127.0.0.1:5000/crawl", {
